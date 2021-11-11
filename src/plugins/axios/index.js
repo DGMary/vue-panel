5237753d2dbf07bdf91558b215a64a6f8754ac97
Fiasko <snakeice2000@mail.ru>
2021-11-11 16:02:43 +0300

    Правила оформления instance

--- src/plugins/axios/index.js
+++ src/plugins/axios/index.js
@@ -1,5 +1,16 @@
 import axios from "axios";
+// Сюда лучше добавить option 
+/* Для axios это будет выглядеть так
 
+const options = {
+  // В baseURL вставляем либо прямую строку сервер либо забираем из env
+  baseURL: 'http://localhost:1234' // Это пример, теперь эта ссылка будет вставляться в каждый запрос
+}
+
+Все запросы instance должны выглядеть так:
+instance.get('/result')...
+
+*/
 const instance = axios.create({});
 
 export default instance;
